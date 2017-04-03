7bde81a10c113679b40e40b5ef5fb749d079d891
Mike Mamaenko <mmamaenko@yahoo.com>
2017-04-02 20:13:29 -0700

    VirtualEnv Stage #1. Install prerequisites

--- roles/test/integration/default/serverspec/default_spec.rb
+++ roles/test/integration/default/serverspec/default_spec.rb
@@ -1,13 +1,19 @@
 require 'serverspec'
 set :backend, :exec
 
-describe group('mike') do
+vars = YAML.load_file('/tmp/kitchen/roles/devops/defaults/main.yml')
+describe group(vars['local_user']) do
    it { is_expected.to exist }
 end
 
-describe user('mike') do
+describe user(vars['local_user']) do
     it { is_expected.to exist }
     it { is_expected.to belong_to_group 'mike' }
     it { is_expected.to have_login_shell '/bin/bash' }
 end
 
+#check that .ssh has right permissions
+#describe command("ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no gitlab ps") do
+#    let(:sudo_options) { '-u mike -i' }
+#    its(:stdout) { should contain('gitlab') }
+#end
